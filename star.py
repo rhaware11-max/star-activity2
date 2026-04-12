aabe5a57483904998953fade3972a55db628540b
rhaware11-max <rhaware11@gmail.com>
2026-04-12 13:47:13 -0400

    Add files via upload

---
+++ star.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("light pink")
+v=turtle.Turtle()
+#first triangle
+v.forward(100)
+v.left(120)
+v.forward(100)
+
+v.left(120)
+v.forward(100)
+
+v.penup()
+v.right(150)
+v.forward(50)
+
+#second triangle
+v.pendown()
+v.right(90)
+v.forward(100)
+
+v.right(120)
+v.forward(100)
+
+v.right(120)
+v.forward(100)
+
+turtle.done()
